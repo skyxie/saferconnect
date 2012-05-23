bed1162566dfd111d47e117071406d2e6dba84ee
Tian Xie <tian@animoto.com>
2012-05-23 04:00:49 -0400

    Adding Foursquare profile view

--- lib/foursquare_connect.rb
+++ lib/foursquare_connect.rb
@@ -4,28 +4,44 @@ module FoursquareConnect
   CLIENT_SECRET = SERVICES[:foursquare][:secret]
   REDIRECT_URI = SERVICES[:foursquare][:redirect_uri]
   ACCESS_TOKEN_URI = "https://foursquare.com/oauth2/access_token"
+  SELF_PROFILE_URI = "https://api.foursquare.com/v2/users/self"
 
-  def self.request_access_token(code)
-    request_params = {
-      :client_id => CLIENT_ID,
-      :client_secret => CLIENT_SECRET,
-      :grant_type => "authorization_code",
-      :redirect_uri => REDIRECT_URI,
-      :code => code
-    }.inject([]) { |list,pair| list + [pair.join("=")] }.join("&")
+  class << self
 
-    uri = URI.parse("#{ACCESS_TOKEN_URI}?#{request_params}")
+    def request_access_token(code)
+      request_params = {
+        :client_id => CLIENT_ID,
+        :client_secret => CLIENT_SECRET,
+        :grant_type => "authorization_code",
+        :redirect_uri => REDIRECT_URI,
+        :code => code
+      }.inject([]) { |list,pair| list + [pair.join("=")] }.join("&")
 
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      uri = URI.parse("#{ACCESS_TOKEN_URI}?#{request_params}")
 
-    req = Net::HTTP::Get.new(uri.request_uri)
-    resp = http.request(req)
+      resp = http_request(uri)
+      
+      raise "Sorry, we could not authenticate" if resp.code !~ /20[0-9]/
+
+      JSON.parse(resp.body)["access_token"]
+    end
     
-    raise "Sorry, we could not authenticate" if resp.code !~ /20[0-9]/
+    def request_profile(token)
+      uri = URI.parse("#{SELF_PROFILE_URI}?oauth_token=#{token}")
+      resp = http_request(uri)
+      raise resp if resp.code !~ /20[0-9]/
+      JSON.parse(resp.body)["response"]
+    end
 
-    JSON.parse(resp.body)["access_token"]
+    def http_request(uri)
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+      req = Net::HTTP::Get.new(uri.request_uri)
+      http.request(req)
+    end
+
   end
 
   module Helpers
@@ -42,6 +58,10 @@ module FoursquareConnect
     def set_foursquare_access_token(token)
       session[:foursquare_access_token] = token
       logger.debug "Setting Foursquare access token to: #{session[:foursquare_access_token]}"
+    end
+
+    def foursquare_profile
+      @foursquare_profile ||= FoursquareConnect.request_profile(foursquare_access_token)
     end
   end
 end
